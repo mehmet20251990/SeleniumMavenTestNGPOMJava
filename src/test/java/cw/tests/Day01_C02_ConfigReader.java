0858577ecc6db4272b4f5999a30007636b091424
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-06 20:48:52 +0300

    update-1

---
+++ src/test/java/cw/tests/Day01_C02_ConfigReader.java
@@ -0,0 +1,13 @@
+package cw.tests;
+
+import cw.utilities.ConfigReader;
+import cw.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class Day01_C02_ConfigReader {
+    @Test
+    public void configReader() {
+        Driver.getDriver().get(ConfigReader.getProperty("google_url"));
+        Driver.closeDriver();
+    }
+}
