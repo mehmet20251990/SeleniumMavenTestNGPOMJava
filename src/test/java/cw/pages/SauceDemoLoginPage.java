2cf9d83e22363ad8f866d565a6b486480a838f85
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-06 21:27:21 +0300

    update-2

---
+++ src/test/java/cw/pages/SauceDemoLoginPage.java
@@ -0,0 +1,20 @@
+package cw.pages;
+
+import cw.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SauceDemoLoginPage {
+    // initElements methodu Page class i initialize etmek adina kullanilir
+    public SauceDemoLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    // @FindBy annotasyonu elementleri locate ederken kullandigimiz ve initElements methodu ile elementlerin bulunmasini saglayan bir anotasyondur
+    @FindBy(id = "user-name")
+    public WebElement txtUsername;
+    @FindBy(id = "password")
+    public WebElement txtPassword;
+    @FindBy(xpath = "//*[@id='login-button']")
+    public WebElement btnLogin;
+}
---
+++ src/test/java/cw/tests/Day02_C02_PageClass.java
@@ -0,0 +1,5 @@
+package cw.tests;
+
+public class Day02_C02_PageClass {
+
+}
